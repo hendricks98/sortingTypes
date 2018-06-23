3512d02bd5f6409e7b867ba351e12d662bc2b781
Jamel Hendricks <jamelmhendricks@gmail.com>
2018-06-22 20:36:30 -0400

    finalize bubble sort, add analytics for bubble sort

--- bubbleSort/bubbleSort.java
+++ bubbleSort/bubbleSort.java
@@ -1,21 +1,40 @@
 import java.lang.Math;
+import java.text.DecimalFormat;
 
 public class bubbleSort {
 	
+	static long totalCount;
+	static double start;
+	static double finish;
 
 	public static void main(String[] args) {
 
 
-			System.out.println("Generating unsorted array: ");
+		System.out.println("Generating unsorted array: ");
 
-			int[] testNums = fillArray();
-			printArray(testNums);
-			System.out.println("");
+		int[] testNums = fillArray();
+		printArray(testNums);
+		System.out.println("");
 
-			System.out.println("Running bubbleSort algorithm: ");
+		System.out.println("Running bubbleSort algorithm: ");
 
-			printArray(bubbleSort(testNums));
+		start = System.nanoTime() / 1000000000.0;
+		printArray(bubbleSort(testNums));
+		finish = System.nanoTime() / 1000000000.0;
+		double totalTime = (finish - start);
 
+		System.out.println("");
+
+		DecimalFormat fourDec = new DecimalFormat(".0000");
+		DecimalFormat twoDec = new DecimalFormat(".00");
+		double time = Double.parseDouble(fourDec.format(totalTime));
+		double swapRatio = Double.parseDouble(twoDec.format(totalCount / time));
+
+		System.out.println("Analytics: ");
+		System.out.println("Number of numbers: " + testNums.length);
+		System.out.println("Number of swaps: " + totalCount);
+		System.out.println("Approximate processing time: " + time + " seconds");
+		System.out.println("Swaps per second: " + swapRatio );
 
 
 	}
@@ -33,12 +52,12 @@ public class bubbleSort {
 
 	public static int[] fillArray() {
 		int[] numbers;
-		numbers = new int[50];
+		numbers = new int[100];
 
 		// fill array with random numbers between 1-100
 		for (int i = 0; i < numbers.length; i++){
 
-			numbers[i] = (int) (Math.random() * 100 ) + 1;
+			numbers[i] = (int) (Math.random() * 1000 ) + 1;
 		}
 
 		return numbers;
@@ -62,6 +81,7 @@ public class bubbleSort {
 					numbers[i] = swap;
 					//printArray(numbers);
 					swapCount++;
+					totalCount++;
 
 				}
 
